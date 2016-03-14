77f2d1ee4e3bab4bb778116b8da848fbca5fae4b
Chris Kastorff <encryptio@gmail.com>
2016-03-14 16:50:55 -0700

    initial commit

---
+++ cache.go
@@ -0,0 +1,264 @@
+// package meetup implements a cache with meetup and smart revalidation.
+//
+// A meetup cache will coalesce concurrent requests for the same item, so that
+// only one request will be done to the backend per entry.
+//
+// In addition, this package implements a rich expiration state machine which
+// helps to avoid latency spikes and provides backpressure when the backend
+// returns errors.
+//
+// For example, if you have a dynamic proxy which proxies many hostnames to
+// different places, you'll want an Options configuration similar to this:
+//     op := Options{
+//         Get: ...,
+//
+//         // Don't overload the backend with more than 50 requests at a time
+//         Concurrency: 50,
+//
+//         // Keep errors for 2 seconds so that a failed backend doesn't get
+//         // slammed with huge request load, and so that missing mappings don't
+//         // cause each user request to cause a backend request.
+//         //
+//         // Additionally, keeping ErrorAge relatively low means that we can
+//         // recover from backend failures somewhat quickly.
+//         ErrorAge: 2 * time.Second,
+//
+//         // Keep entries for a long time so that a failed backend doesn't
+//         // cause production traffic to quickly fail. We handle updating
+//         // mappings with revalidation instead.
+//         ExpireAge: time.Hour,
+//
+//         // Revalidate entries regularly so that we know their mapping points
+//         // to the right place.
+//         RevalidateAge: time.Minute,
+//     }
+package meetup
+
+import (
+	"errors"
+	"sync"
+	"time"
+
+	"gopkg.in/tomb.v2"
+)
+
+var (
+	// ErrClosed is returned by Cache.Get when the Cache has been Closed.
+	ErrClosed = errors.New("Cache Closed")
+)
+
+var (
+	// now is a variable so that expiration and revalidation tests can be written
+	// deterministically.
+	now = time.Now
+
+	// postGetCheckCh, if non-nil, has one value written to it per (*Cache).Get
+	// that finishes all its checks and starts any fill calls.
+	postGetCheckCh chan struct{}
+
+	// fillComplete, if non-nil, has one value written to it per (*Cache).fill
+	// that completes.
+	fillComplete chan struct{}
+)
+
+// Options control the behavior of the Cache with respect to its backend.
+type Options struct {
+	// When a key is requested that does not exist in the cache (or needs to be
+	// revalidated) then Get will be called. Get is called concurrently, at most
+	// once concurrently per concurrent key requested.
+	Get func(key string) (interface{}, error)
+
+	// If greater than zero, only Concurrency Get calls will be done
+	// concurrently. Any other calls will wait until one of the running Get calls
+	// complete.
+	Concurrency int
+
+	// If an Options.Get returns an error, cache the error for this amount of
+	// time. If negative or zero, don't cache errors.
+	ErrorAge time.Duration
+
+	// Once an entry's age reaches ExpireAge, it is considered expired and the
+	// cached result will not be used. If set to zero, values do not expire.
+	ExpireAge time.Duration
+
+	// Once an entry's age reaches RevalidateAge, a background Options.Get will
+	// be made on its key, but Cache.Get will continue to return immediately. If
+	// the background Get returns an error, it will not be retried until
+	// ErrorAge has passed since the last revalidation attempt.
+	//
+	// If zero, revalidation is disabled.
+	RevalidateAge time.Duration
+}
+
+type Cache struct {
+	o Options
+
+	concLimitCh chan struct{}
+
+	// mu protects m, but NOT its entries; each entry has its own lock.
+	mu sync.RWMutex
+	m  map[string]*entry
+
+	t tomb.Tomb
+}
+
+type entry struct {
+	// mu protects all fields in the entry
+	mu        sync.Mutex
+	readyCond *sync.Cond
+
+	LastUpdate time.Time
+
+	Filling bool
+
+	// Only set this through SetReady
+	Ready bool
+
+	// Value and Error are valid iff Ready is true
+	Value interface{}
+	Error error
+
+	// Running is true iff a fill is running for this entry
+	Running bool
+}
+
+// You must hold e.mu when calling SetReady
+func (e *entry) SetReady(r bool) {
+	if e.Ready != r {
+		e.readyCond.Broadcast()
+	}
+	e.Ready = r
+}
+
+// NewCache returns a Cache with the given Options.
+func NewCache(o Options) *Cache {
+	c := &Cache{
+		o: o,
+		m: make(map[string]*entry),
+	}
+
+	if o.Concurrency > 0 {
+		c.concLimitCh = make(chan struct{}, o.Concurrency)
+	}
+
+	// Keep the tomb alive for future c.fill calls
+	c.t.Go(func() error {
+		<-c.t.Dying()
+		return nil
+	})
+
+	return c
+}
+
+// Get retrieves an entry's value from the cache, calling Options.Get if needed
+// to fill the cache. If multiple concurrent Get calls occur on the same key,
+// all of them will recieve the return value of a single Options.Get call.
+func (c *Cache) Get(key string) (interface{}, error) {
+	select {
+	case <-c.t.Dying():
+		return nil, ErrClosed
+	default:
+	}
+
+	t := now()
+
+	c.mu.RLock()
+	e, ok := c.m[key]
+	if ok {
+		c.mu.RUnlock()
+	} else {
+		// No entry for this key.
+		c.mu.RUnlock()
+		c.mu.Lock()
+		e, ok = c.m[key]
+		if !ok {
+			// Still no entry for this key. Create the entry.
+			e = &entry{}
+			e.readyCond = sync.NewCond(&e.mu)
+			c.startFill(key, e) // NB: don't need e.mu, no other goroutine can have e yet
+			c.m[key] = e
+		}
+		c.mu.Unlock()
+	}
+
+	e.mu.Lock()
+	age := t.Sub(e.LastUpdate)
+	if c.o.ExpireAge > 0 && age >= c.o.ExpireAge {
+		// This entry has expired. Clear its value and make sure it's filling.
+		e.SetReady(false)
+		e.Value = nil
+		e.Error = nil
+		if !e.Filling {
+			c.startFill(key, e)
+		}
+	}
+	if e.Ready {
+		if e.Error != nil && age >= c.o.ErrorAge {
+			e.SetReady(false)
+			e.Value = nil
+			e.Error = nil
+			if !e.Filling {
+				c.startFill(key, e)
+			}
+		} else if c.o.RevalidateAge > 0 && age >= c.o.RevalidateAge && !e.Filling {
+			c.startFill(key, e)
+		}
+	}
+
+	// Used for the test suite.
+	if postGetCheckCh != nil {
+		postGetCheckCh <- struct{}{}
+	}
+
+	for !e.Ready {
+		e.readyCond.Wait()
+	}
+
+	value := e.Value
+	err := e.Error
+
+	e.mu.Unlock()
+
+	if err != nil {
+		return nil, err
+	} else {
+		return value, nil
+	}
+}
+
+func (c *Cache) startFill(key string, e *entry) {
+	e.Filling = true
+	c.t.Go(func() error {
+		c.fill(key, e)
+		return nil
+	})
+}
+
+func (c *Cache) fill(key string, e *entry) {
+	if fillComplete != nil {
+		defer func() { fillComplete <- struct{}{} }()
+	}
+
+	if c.concLimitCh != nil {
+		c.concLimitCh <- struct{}{}
+		defer func() { <-c.concLimitCh }()
+	}
+
+	t := now()
+	value, err := c.o.Get(key)
+
+	e.mu.Lock()
+	e.LastUpdate = t
+	e.Value = value
+	e.Error = err
+	e.Filling = false
+	e.SetReady(true)
+	e.mu.Unlock()
+}
+
+// Close waits for all running Options.Get calls to finish and makes all future
+// Cache.Get calls return ErrClosed.
+func (c *Cache) Close() error {
+	c.t.Kill(nil)
+	return c.t.Wait()
+}
---
+++ cache_test.go
@@ -0,0 +1,378 @@
+package meetup
+
+import (
+	"errors"
+	"reflect"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+)
+
+var (
+	fakeTimeMu sync.Mutex
+	fakeTime   = time.Now()
+)
+
+func init() {
+	now = func() time.Time {
+		fakeTimeMu.Lock()
+		t := fakeTime
+		fakeTimeMu.Unlock()
+		return t
+	}
+}
+
+func advanceTime(d time.Duration) {
+	fakeTimeMu.Lock()
+	fakeTime = fakeTime.Add(d)
+	fakeTimeMu.Unlock()
+}
+
+func mustGet(t *testing.T, c *Cache, key string, want interface{}) {
+	v, err := c.Get(key)
+	if err != nil {
+		t.Errorf("Get(%#v) returned unexpected error %v", key, err)
+	}
+	if !reflect.DeepEqual(v, want) {
+		t.Errorf("Get(%#v) = %#v, but wanted %#v", key, v, want)
+	}
+}
+
+func TestCache(t *testing.T) {
+	hits := 0
+	c := NewCache(Options{
+		Get: func(key string) (interface{}, error) {
+			hits++
+			return key, nil
+		},
+	})
+	defer c.Close()
+
+	if hits != 0 {
+		t.Fatalf("hits != 0 after init")
+	}
+
+	mustGet(t, c, "a", "a")
+	if hits != 1 {
+		t.Fatalf("hits != 1 after first use")
+	}
+
+	mustGet(t, c, "b", "b")
+	if hits != 2 {
+		t.Fatalf("hits = %v after second use", hits)
+	}
+
+	mustGet(t, c, "a", "a")
+	if hits != 2 {
+		t.Fatalf("hits != 2 after third use")
+	}
+}
+
+func TestExpiry(t *testing.T) {
+	var mu sync.Mutex
+	hits := 0
+
+	c := NewCache(Options{
+		Get: func(key string) (interface{}, error) {
+			mu.Lock()
+			hits++
+			mu.Unlock()
+			return key, nil
+		},
+		ExpireAge: time.Second,
+	})
+	defer c.Close()
+
+	mustGet(t, c, "a", "a")
+	mu.Lock()
+	if hits != 1 {
+		t.Fatalf("hits = %v after first use", hits)
+	}
+	mu.Unlock()
+
+	advanceTime(2 * time.Second)
+
+	mustGet(t, c, "a", "a")
+	mu.Lock()
+	if hits != 2 {
+		t.Fatalf("hits != 2 after second use")
+	}
+	mu.Unlock()
+
+	advanceTime(time.Second / 2)
+
+	mustGet(t, c, "a", "a")
+	mu.Lock()
+	if hits != 2 {
+		t.Fatalf("hits = %v after third use", hits)
+	}
+	mu.Unlock()
+}
+
+func TestExpiryUsesStartTime(t *testing.T) {
+	hits := 0
+
+	c := NewCache(Options{
+		Get: func(key string) (interface{}, error) {
+			advanceTime(time.Second)
+			hits++
+			return key, nil
+		},
+		ExpireAge: time.Second,
+	})
+	defer c.Close()
+
+	mustGet(t, c, "a", "a")
+	if hits != 1 {
+		t.Fatalf("hits != 1 after first use")
+	}
+
+	mustGet(t, c, "a", "a")
+	if hits != 2 {
+		t.Fatalf("hits = %v after second use", hits)
+	}
+}
+
+func TestMeetup(t *testing.T) {
+	postGetCheckCh = make(chan struct{})
+	defer func() { postGetCheckCh = nil }()
+
+	const concurrency = 1000
+
+	blockGets := newBoolWatcher(true)
+
+	var mu sync.Mutex
+	hits := 0
+
+	c := NewCache(Options{
+		Get: func(key string) (interface{}, error) {
+			blockGets.Wait(false)
+
+			mu.Lock()
+			c := hits
+			hits++
+			mu.Unlock()
+			return c, nil
+		},
+	})
+	defer c.Close()
+
+	vals := make(chan interface{})
+	for i := 0; i < concurrency; i++ {
+		go func() {
+			v, err := c.Get("a")
+			if err != nil {
+				t.Errorf(`c.Get("a") returned error %v`, err)
+			}
+			vals <- v
+		}()
+	}
+
+	for i := 0; i < concurrency; i++ {
+		<-postGetCheckCh
+	}
+
+	blockGets.Set(false)
+
+	for i := 0; i < concurrency; i++ {
+		v := <-vals
+		if !reflect.DeepEqual(v, 0) {
+			t.Errorf("Cache did not meet up. Wanted value %v, got %v", 0, v)
+		}
+	}
+}
+
+func TestConcurrencyLimit(t *testing.T) {
+	blockGets := newBoolWatcher(true)
+
+	const (
+		workers = 1000
+		limit   = 3
+	)
+
+	var mu sync.Mutex
+	conc := 0
+	maxConc := 0
+
+	c := NewCache(Options{
+		Get: func(key string) (interface{}, error) {
+			mu.Lock()
+			conc++
+			if conc > maxConc {
+				maxConc = conc
+			}
+			mu.Unlock()
+
+			blockGets.Wait(false)
+
+			mu.Lock()
+			conc--
+			mu.Unlock()
+
+			return key, nil
+		},
+		Concurrency: limit,
+	})
+	defer c.Close()
+
+	vals := make(chan interface{})
+
+	for i := 0; i < workers; i++ {
+		go func(i int) {
+			k := strconv.FormatInt(int64(i), 10)
+			v, _ := c.Get(k)
+			vals <- v
+		}(i)
+	}
+
+	blockGets.Set(false)
+
+	for i := 0; i < workers; i++ {
+		<-vals
+	}
+
+	if conc != 0 {
+		t.Errorf("Options.Get still running after Cache.Get returned")
+	}
+
+	t.Logf("max concurrency seen was %v", maxConc)
+	if maxConc > limit {
+		t.Errorf("max concurrency of %v is over limit %v", maxConc, limit)
+	}
+}
+
+func TestCacheDoesntKeepErrors(t *testing.T) {
+	deliberateErr := errors.New("deliberate failure")
+
+	hits := 0
+	c := NewCache(Options{
+		Get: func(key string) (interface{}, error) {
+			hits++
+			return nil, deliberateErr
+		},
+	})
+	defer c.Close()
+
+	v, err := c.Get("a")
+	if v != nil {
+		t.Errorf("Got unexpected value %#v from c.Get", v)
+	}
+	if err != deliberateErr {
+		t.Errorf("Got unexpected error %v from c.Get", err)
+	}
+
+	v, err = c.Get("a")
+	if v != nil {
+		t.Errorf("Got unexpected value %#v from c.Get", v)
+	}
+	if err != deliberateErr {
+		t.Errorf("Got unexpected error %v from c.Get", err)
+	}
+
+	if hits != 2 {
+		t.Errorf("Error was cached")
+	}
+}
+
+func TestRevalidation(t *testing.T) {
+	fillComplete = make(chan struct{})
+	defer func() { fillComplete = nil }()
+
+	blockGets := newBoolWatcher(false)
+	var getReadyToBlock chan struct{}
+
+	var mu sync.Mutex
+	hits := 0
+	finished := 0
+
+	c := NewCache(Options{
+		Get: func(key string) (interface{}, error) {
+			mu.Lock()
+			hits++
+			myHits := hits
+			mu.Unlock()
+			if getReadyToBlock != nil {
+				getReadyToBlock <- struct{}{}
+			}
+			blockGets.Wait(false)
+			mu.Lock()
+			finished++
+			mu.Unlock()
+			return myHits, nil
+		},
+		RevalidateAge: time.Second,
+	})
+	defer c.Close()
+
+	mustGet(t, c, "a", 1)
+	<-fillComplete
+	if hits != 1 {
+		t.Errorf("hits != 1")
+	}
+
+	blockGets.Set(true)
+	advanceTime(time.Second)
+
+	getReadyToBlock = make(chan struct{}, 1)
+	mustGet(t, c, "a", 1) // NB: does not block with background revalidation
+	<-getReadyToBlock
+	mu.Lock()
+	if hits != 2 {
+		t.Errorf("hits = %v, wanted 2", hits)
+	}
+	if finished != 1 {
+		t.Errorf("finished != 1")
+	}
+	mu.Unlock()
+
+	blockGets.Set(false)
+	<-fillComplete
+
+	getReadyToBlock = nil
+	mustGet(t, c, "a", 2)
+}
+
+func TestErrorCaching(t *testing.T) {
+	deliberate := errors.New("deliberate failure")
+
+	var mu sync.Mutex
+	hits := 0
+
+	c := NewCache(Options{
+		Get: func(key string) (interface{}, error) {
+			mu.Lock()
+			hits++
+			mu.Unlock()
+			return nil, deliberate
+		},
+		ErrorAge: time.Second,
+	})
+	defer c.Close()
+
+	_, err := c.Get("a")
+	if err != deliberate {
+		t.Errorf("Got unexpected error %v from Get", err)
+	}
+
+	_, err = c.Get("a")
+	if err != deliberate {
+		t.Errorf("Got unexpected error %v from Get", err)
+	}
+
+	if hits != 1 {
+		t.Errorf("error was not cached")
+	}
+
+	advanceTime(time.Second)
+
+	deliberate = errors.New("other deliberate failure")
+	_, err = c.Get("a")
+	if err != deliberate {
+		t.Errorf("Got unexpected error %v from Get", err)
+	}
+
+	if hits != 2 {
+		t.Errorf("hits = %v", hits)
+	}
+}
